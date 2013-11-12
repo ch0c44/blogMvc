27396f74c9107ff0f705a3dff699980171d6e00c
ch0c44 <ch0c4.sampedro@gmail.com>
2013-11-12 07:45:29 +0100

    add files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+define('ROOT', __DIR__);
+define('APP', __DIR__ . '/app/');
+define('WEBROOT', __DIR__ . '/app/webroot');
+define('CONF', __DIR__ . '/app/webroot/config/');
+define('ROUTE', __DIR__ . 'app/route/')
+
+require ROOT . '/vendor/autoload.php';
+
+use RedBean_Facade as R;
+
+R::setup('mysql:host=localhost;dbname=blog', 'root','azeqsd'); 
+
+include CONF . 'twig.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () use ($app, $twig)
+{
+	
+});
+
+$app->run();
