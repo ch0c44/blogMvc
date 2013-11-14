ea2f31412af5e1c72d4ecc6dfece884f2f25f8ff
ch0c4 <johan.sampedro@gmail.com>
2013-11-14 17:51:03 +0100

    ajout du style

--- app/webroot/config/twig.php
+++ app/webroot/config/twig.php
@@ -4,3 +4,9 @@ $loader = new Twig_Loader_Filesystem(APP . 'view/');
 $twig = new Twig_Environment($loader, array(
     'cache' => WEBROOT . 'cache/',
 ));
+
+$twig->addGlobal('CSS', WEBROOT .'css/');
+$twig->addGlobal('JS', WEBROOT .'js/');
+$twig->addGlobal('IMG', WEBROOT .'img/');
+
+$twig->clearCacheFiles();
--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 define('ROOT', __DIR__);
 define('APP', __DIR__ . '/app/');
-define('WEBROOT', __DIR__ . '/app/webroot');
+define('WEBROOT', __DIR__ . '/app/webroot/');
 define('CONF', __DIR__ . '/app/webroot/config/');
-define('ROUTE', __DIR__ . 'app/route/')
+define('ROUTE', __DIR__ . 'app/route/');
 
 require ROOT . '/vendor/autoload.php';
 
@@ -18,7 +18,7 @@ $app = new \Slim\Slim();
 
 $app->get('/', function () use ($app, $twig)
 {
-	
+	echo $twig->render('layout/main.html.twig');
 });
 
 $app->run();
